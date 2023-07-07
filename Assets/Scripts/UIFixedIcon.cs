5f93301bcb1f7933b1807871646c1fee82c78a6d
imhe6 <95729258+imhe6@users.noreply.github.com>
2023-07-07 20:29:47 +0800

    Update UIFixedIcon

--- Assets/Scripts/UIFixedIcon.cs
+++ Assets/Scripts/UIFixedIcon.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class UIFixedIcon : MonoBehaviour
 {
@@ -12,24 +14,25 @@ public class UIFixedIcon : MonoBehaviour
     RectTransform rectTrans;// UI elements following the object
     public Vector2 offset = new(-1, 1);
     public Vector2 spacing = new(0.5f, 0f);
-    [SerializeField]
-    RectTransform[] m_IconPrefab;
+
+    public List<GameObject> m_iconPrefab;
 
     Canvas m_canvas;
 
-    RectTransform[] m_thisIcons;
+    public List<GameObject> m_displayIcons;
 
-    // Start is called before the first frame update
+    // Start is called before the first fr1ame update
     void Start()
     {
         Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
         m_canvas = FindObjectOfType<Canvas>();
-        for (int i = 0; i < m_IconPrefab.Length; i++)
+        for (int i = 0; i < m_iconPrefab.Count; i++)
         {
             var position = screenPos + offset + spacing * i;
-            m_thisIcons[i] = Instantiate(m_IconPrefab[i]);
-            m_thisIcons[i].position = position;
-            m_thisIcons[i].SetParent(m_canvas.transform);
+            var icon = Instantiate(m_iconPrefab[i], m_canvas.transform);
+            icon.transform.position = position;
+            m_displayIcons.Add(icon);
+            
         }
     }
 
@@ -37,18 +40,27 @@ public class UIFixedIcon : MonoBehaviour
     void Update()
     {
         Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
-        for (int i = 0; i < m_IconPrefab.Length; i++)
+        for (int i = 0; i < m_iconPrefab.Count; i++)
         {
             var position = screenPos + offset + spacing * i;
-            m_thisIcons[i].position = position;
+            m_displayIcons[i].transform.position = position;
         }
     }
 
     void OnDestroy()
     {
-        foreach (var icon in m_thisIcons)
+        foreach (var icon in m_displayIcons)
         {
             Destroy(icon.gameObject);
         }
+    }
+
+    public void AddIcon(GameObject iconPrefab)
+    {
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        m_iconPrefab.Add(iconPrefab);
+        var icon = Instantiate(iconPrefab, m_canvas.transform);
+        icon.transform.position = screenPos + offset + spacing * (m_displayIcons.Count + 1);
+        m_displayIcons.Add(icon);
     }
 }
